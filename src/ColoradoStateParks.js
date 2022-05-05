eb5050b1ecaa2f2fec24e02563f5106cfaf6843a
jacob <jacobmjones17@gmail.com>
2022-05-05 11:38:59 -0400

    commit tree ad1f1133671b7d3831f6a280c3f3757782ae3387

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+// import howManyParks from "./parks/howManyParks";
+import MesaVerde from "/.parks/MesaVerde";
+// import * as RMFunctions from "./parks/RockyMountain";
+
+// function ColoradoStateParks() {
+//   howManyParks(); // => "42 parks!"
+
+//   return <h1>Colorado State Parks!</h1>;
+// }
+
+// console.log(trees);
+// wildlife();
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  )
 }
+
+export default ColoradoStateParks;
